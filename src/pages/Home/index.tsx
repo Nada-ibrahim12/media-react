72cb17eaf9418e3ecfc73169cdd2cb9a954b8413
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2025-06-14 06:12:14 +0300

    fix imports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MediaCard from "./components/MediaCard";
 import axios from "axios";
 
--- src/pages/Register/components/Register.tsx
+++ src/pages/Register/components/Register.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Joi from "joi";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const navigate = useNavigate();
